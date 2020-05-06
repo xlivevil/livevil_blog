321782da79d7635a76c91353edf728c4296641a4
xliveivl <xlivevil@aliyun.com>
2020-05-06 23:06:21 +0800

    livevil

--- api/views.py
+++ api/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.settings import api_settings
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.views import APIView
+from rest_framework.views import APIView
+from rest_framework.views import APIView
+from rest_framework.views import APIView
+from rest_framework.views import APIView
 from rest_framework.views import APIView
 
 
 class View(APIView):
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    parser_classes = api_settings.DEFAULT_PARSER_CLASSES
+    authentication_classes = api_settings.DEFAULT_AUTHENTICATION_CLASSES
+    throttle_classes = api_settings.DEFAULT_THROTTLE_CLASSES
+    permission_classes = api_settings.DEFAULT_PERMISSION_CLASSES
+    content_negotiation_class = api_settings.DEFAULT_CONTENT_NEGOTIATION_CLASS
+    metadata_class = api_settings.DEFAULT_METADATA_CLASS
+    versioning_class = api_settings.DEFAULT_VERSIONING_CLASS
 
     def get(self):
         return
