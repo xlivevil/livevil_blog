b7dde896ffa31528656cceec6471dea5dd72d367
xliveivl <xlivevil@aliyun.com>
2020-05-10 14:48:42 +0800

    style: 💄 use CDN

--- livevil_blog/settings/production.py
+++ livevil_blog/settings/production.py
@@ -4,7 +4,7 @@ from .common import *
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['xlivevil.com']
 
