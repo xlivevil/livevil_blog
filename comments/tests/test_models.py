c63eb5019d6c263923cf12a6e2dc0dcaa825c622
xlivevil <xlivevil@aliyun.com>
2021-12-29 17:30:48 +0800

    ✅ Test(test comment): add test_model in comments

--- comments/tests/test_models.py
+++ comments/tests/test_models.py
@@ -22,5 +22,25 @@ class PostCommentModelTestCase(CommentDataTestCase):
     def test_str_representation(self):
         self.assertEqual(self.comment.__str__(), 'admin: 评论内容...')
 
+    def test_auto_populate_modified_time(self):
+        self.assertIsNotNone(self.comment.modified_time)
 
-# TODO：测试子评论
+        old_comment_modified_time = self.comment.modified_time
+        self.comment.comment = '新的评论内容'
+        self.comment.save()
+        self.comment.refresh_from_db()
+        self.assertTrue(self.comment.modified_time > old_comment_modified_time)
+
+    def test_subcomment(self):
+        sub_comment = PostComment.objects.create(
+            user=self.user,
+            user_email='a@a.com',
+            comment='子评论内容',
+            content_type=self.post_content_type,
+            object_pk=str(self.post.pk),
+            site_id=getattr(settings, 'SITE_ID', None),
+            parent=self.comment,
+            submit_date=timezone.now()
+        )
+        self.assertEqual(sub_comment.parent, self.comment)
+        self.assertEqual(self.comment.children.count(), 1)
